17e4b479da8b51bfe45c529732ce5bcd254f1a80
medfahmy <medfahmy99@gmail.com>
2021-05-19 12:55:39 +0100

    users router

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://med:med@cluster0.i14ji.mongodb.net/socialnetwork?retryWrites=true&w=majority",
+  secretOrKey: "secret",
 };
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body parser middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
@@ -16,7 +21,13 @@ mongoose
   .then(() => console.log("mongodb connected"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("hello there"));
+// app.get("/", (req, res) => res.send("hello there"));
+
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
