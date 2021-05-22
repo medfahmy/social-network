f0f42cf5a72e155594f3b67efcbf59b64dc99991
medfahmy <medfahmy99@gmail.com>
2021-05-22 14:08:20 +0100

    update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 
-const users = require("./routes/api/user");
-const profiles = require("./routes/api/profile");
-const posts = require("./routes/api/post");
+const user = require("./routes/api/user");
+const profile = require("./routes/api/profile");
+const post = require("./routes/api/post");
 
 const app = express();
 
@@ -18,7 +18,7 @@ const db = require("./config/keys").mongoURI;
 // connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("mongodb connected"))
+  .then(() => console.log(`****  mongodb connected ****`))
   .catch((err) => console.log(err));
 
 // app.get("/", (req, res) => res.send("hello there"));
@@ -30,10 +30,14 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 // Routes
-app.use("/api/user", users);
-app.use("/api/profile", profiles);
-app.use("/api/post", posts);
+app.use("/api/user", user);
+app.use("/api/profile", profile);
+app.use("/api/post", post);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`
+****   server running on port ${port} ****
+`)
+);
