1e908352f03fe10654c321d98e9c5b031776812a
medfahmy <medfahmy99@gmail.com>
2021-06-02 11:53:03 +0100

    initial react components

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <div>
+          <a href="landing.html">Social Network</a>
+          <button>
+            <span />
+          </button>
+
+          <div>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
