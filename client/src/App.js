a7777ab47e8175c0f46764bb36031c6e8f67053b
mohamedfahmy-dt <mohamedfahmy@digitrasform.co>
2021-06-09 13:00:36 +0100

    Revert "implementing redux, creating register action"
    This reverts commit b23805247a1a7289a880fe007738ffbc46adb507.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,25 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
-import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import "./App.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </div>
-          <Footer />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
         </div>
-      </Router>
-    </Provider>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-
-import { registerUser } from "../../actions/authActions";
 
 class Register extends Component {
   constructor() {
@@ -18,12 +13,6 @@ class Register extends Component {
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
   }
 
   onChange(e) {
@@ -41,23 +30,12 @@ class Register extends Component {
       password2: this.state.password2,
     };
 
-    this.props.registerUser(newUser, this.props.history);
-
-    // console.log(newUser);
-    // axios
-    //   .post("api/user/register", newUser)
-    //   .then((res) => console.log(res.data))
-    //   .catch((err) => this.setState({ errors: err.response.data }));
+    console.log(newUser);
   }
 
   render() {
-    // const user = this.props.auth.user;
-    const errors = this.state.errors;
-
     return (
       <div className="register">
-        {/* <pre>{user ? JSON.stringify(user, null, 2) : false}</pre> */}
-        <pre>{errors ? JSON.stringify(errors, null, 2) : false}</pre>
         <div className="container">
           <div className="row">
             <div className="col">
@@ -102,15 +80,4 @@ class Register extends Component {
   }
 }
 
-Register.propTypes = {
-  registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-  errors: state.errors,
-});
-
-export default connect(mapStateToProps, { registerUser })(withRouter(Register));
+export default Register;
