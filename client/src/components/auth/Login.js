64faee96e1b9daa2802a6fd35bd11264db18c8c4
medfahmy <medfahmy99@gmail.com>
2021-06-09 13:06:45 +0100

    repair This reverts commit a7777ab47e8175c0f46764bb36031c6e8f67053b.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,62 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+    // console.log({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    console.log(user);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>Login</h1>
+              <p>Sign in to your account</p>
+              <form onSubmit={this.onSubmit}>
+                <input
+                  type="email"
+                  placeholder="Email address"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                ></input>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                ></input>
+                <input type="submit"></input>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
